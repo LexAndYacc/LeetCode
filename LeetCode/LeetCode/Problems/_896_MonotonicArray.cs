8943c8ea03c262658995531d5b3df570b4f8b41d
az <westeacc@outlook.com>
2021-02-28 16:03:32 +0800

    _896_MonotonicArray

---
+++ LeetCode/LeetCode/Problems/_896_MonotonicArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _896_MonotonicArray
+    {
+        public bool IsMonotonic(int[] A)
+        {
+            return mySolution(A);
+        }
+
+        bool mySolution(int[] A)
+        {
+            bool result = true;
+
+            if (A != null && A.Length > 2)
+            {
+                int type = 0;
+                for (int i = 0; i < A.Length - 1; i++)
+                {
+                    if (type == 0)
+                    {
+                        if (A[i] - A[i + 1] > 0)
+                        {
+                            type = 1;
+                        }
+                        else if (A[i] - A[i + 1] < 0)
+                        {
+                            type = -1;
+                        }
+                    }
+                    else if (type == 1)
+                    {
+                        if (A[i] - A[i + 1] < 0)
+                        {
+                            result = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+
+                        if (A[i] - A[i + 1] > 0)
+                        {
+                            result = false;
+                            break;
+                        }
+                    }
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_896_MonotonicArrayTest.cs
@@ -0,0 +1,66 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _896_MonotonicArrayTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] A = { 1, 2, 2, 3 };
+            _896_MonotonicArray monotonicArray = new _896_MonotonicArray();
+            bool result = monotonicArray.IsMonotonic(A);
+
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] A = { 6, 5, 4, 4 };
+            _896_MonotonicArray monotonicArray = new _896_MonotonicArray();
+            bool result = monotonicArray.IsMonotonic(A);
+
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] A = { 1, 3, 2 };
+            _896_MonotonicArray monotonicArray = new _896_MonotonicArray();
+            bool result = monotonicArray.IsMonotonic(A);
+
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int[] A = { 1, 2, 4, 5 };
+            _896_MonotonicArray monotonicArray = new _896_MonotonicArray();
+            bool result = monotonicArray.IsMonotonic(A);
+
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int[] A = { 1, 1, 1 };
+            _896_MonotonicArray monotonicArray = new _896_MonotonicArray();
+            bool result = monotonicArray.IsMonotonic(A);
+
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            int[] A = { 1, 1, 2 };
+            _896_MonotonicArray monotonicArray = new _896_MonotonicArray();
+            bool result = monotonicArray.IsMonotonic(A);
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
