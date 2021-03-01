58ac58891b4b066c7ddb81de17842ab8e808bfa5
az <westeacc@outlook.com>
2021-03-01 09:21:03 +0800

    _303_RangeSumQueryImmutable

---
+++ LeetCode/LeetCode/Problems/_303_RangeSumQueryImmutable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _303_RangeSumQueryImmutable
+    {
+        public class NumArray
+        {
+            private int[] sums;
+            public NumArray(int[] nums)
+            {
+                sums = new int[nums.Length + 1];
+                for(int i = 0; i < nums.Length; i++)
+                {
+                    sums[i + 1] = sums[i] + nums[i];
+                }
+            }
+
+            public int SumRange(int i, int j)
+            {
+                return sums[j + 1] - sums[i];
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_303_RangeSumQueryImmutableTest.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static LeetCode.Problems._303_RangeSumQueryImmutable;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _303_RangeSumQueryImmutableTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            NumArray numArray = new NumArray(new int[] { -2, 0, 3, -5, 2, -1 });
+            Assert.AreEqual(1, numArray.SumRange(0, 2));
+            Assert.AreEqual(-1, numArray.SumRange(2, 5));
+            Assert.AreEqual(-3, numArray.SumRange(0, 5));
+        }
+
+    }
+}
