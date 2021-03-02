d784802e04fa87dde944fddde147022a1c19a4c9
shenyuyang <westeacc@dingtalk.com>
2021-03-02 13:42:44 +0800

    _304_RangeSumQuery2dImmutable

---
+++ LeetCode/LeetCode/Problems/_304_RangeSumQuery2dImmutable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _304_RangeSumQuery2dImmutable
+    {
+        public class NumMatrix
+        {
+            int[][] matrixs;
+
+            public NumMatrix(int[][] matrix)
+            {
+                if (matrix.Length == 0)
+                {
+                    matrixs = new int[0][];
+                    return;
+                }
+                else if (matrix[0].Length == 0)
+                {
+                    matrixs = new int[matrix.Length + 1][];
+                    return;
+                }
+
+                matrixs = new int[matrix.Length + 1][];
+                for (int i = 0; i < matrixs.Length; i++)
+                {
+                    matrixs[i] = new int[matrix[0].Length + 1];
+                }
+
+                for (int i = 0; i < matrix.Length; i++)
+                {
+                    for (int j = 0; j < matrix[0].Length; j++)
+                    {
+                        matrixs[i + 1][j + 1] = matrixs[i][j + 1] + (matrixs[i + 1][j] - matrixs[i][j]) + matrix[i][j];
+                    }
+                }
+
+
+            }
+
+
+            public int SumRegion(int row1, int col1, int row2, int col2)
+            {
+                return matrixs[row2 + 1][col2 + 1] - matrixs[row1][col2 + 1] - matrixs[row2 + 1][col1] + matrixs[row1][col1];
+            }
+
+        }
+    }
+
+
+}
---
+++ LeetCode/LeetCodeTest/Problems/_304_RangeSumQuery2dImmutableTest.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static LeetCode.Problems._304_RangeSumQuery2dImmutable;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _304_RangeSumQuery2dImmutableTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[][] matrix =  {
+                new int[] {3, 0, 1, 4, 2},
+                new int[] {5, 6, 3, 2, 1},
+                new int[] {1, 2, 0, 1, 5},
+                new int[] {4, 1, 0, 1, 7},
+                new int[] {1, 0, 3, 0, 5}
+            };
+            NumMatrix obj = new NumMatrix(matrix);
+            Assert.AreEqual(8, obj.SumRegion(2, 1, 4, 3));
+            Assert.AreEqual(11, obj.SumRegion(1, 1, 2, 2));
+            Assert.AreEqual(12, obj.SumRegion(1, 2, 2, 4));
+        }
+
+    }
+}
