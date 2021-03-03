ca78784a5b39a0b01bc63c85e5f1bd3c79d01a8a
shenyuyang <westeacc@dingtalk.com>
2021-03-03 10:58:02 +0800

    _338_CountingBits

---
+++ LeetCode/LeetCode/Problems/_338_CountingBits.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _338_CountingBits
+    {
+        public int[] CountBits(int num)
+        {
+            return mySolution(num);
+        }
+
+        int[] mySolution(int num)
+        {
+            if(num == 0)
+            {
+                return new int[] { 0 };
+            }
+            else if (num == 1)
+            {
+                return new int[] { 0, 1 };
+            }
+
+            int[] result = new int[num + 1];
+            result[0] = 0;
+            result[1] = 1;
+
+            int flag = 2;
+            for (int i = 0; i <= num; i++)
+            {
+                if (i < 2)
+                {
+                    continue;
+                }
+
+                result[i] = result[i - flag] + 1;
+
+                if (i + 1 >= flag * 2)
+                {
+                    flag *= 2;
+                }
+            }
+            return result;
+        }
+
+        int[] bestSolution(int num)
+        {
+            int[] bits = new int[num + 1];
+            for (int i = 1; i <= num; i++)
+            {
+                bits[i] = bits[i >> 1] + (i & 1);
+            }
+            return bits;
+        }
+        int[] bestSolution2(int num)
+        {
+            int[] bits = new int[num + 1];
+            for (int i = 1; i <= num; i++)
+            {
+                bits[i] = bits[i & (i - 1)] + 1;
+            }
+            return bits;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_338_CountingBitsTest.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _338_CountingBitsTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int num = 2;
+            _338_CountingBits countingBits = new _338_CountingBits();
+            int[] result = countingBits.CountBits(num);
+
+            int[] expected = { 0, 1, 1 };
+
+            for(int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int num = 5;
+            _338_CountingBits countingBits = new _338_CountingBits();
+            int[] result = countingBits.CountBits(num);
+
+            int[] expected = { 0, 1, 1, 2, 1, 2 };
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+    }
+}
