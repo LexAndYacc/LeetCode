4633daa0a66d932f5791534ac1243d45e778ee1b
az <westeacc@outlook.com>
2021-03-07 13:19:04 +0800

    _131_PalindromePartitioning

---
+++ LeetCode/LeetCode/Problems/_131_PalindromePartitioning.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _131_PalindromePartitioning
+    {
+        public IList<IList<string>> Partition(string s)
+        {
+            return mySolution(s);
+        }
+
+        IList<IList<string>> mySolution(string s)
+        {
+            List<IList<string>> results = new List<IList<string>>();
+            getPalindrome(results, new List<string>(), s, 0);
+            if (isPalindrome(s, 0, s.Length - 1))
+            {
+                results.Add(new List<string>() { s });
+            }
+            return results;
+        }
+
+        void getPalindrome(List<IList<string>> results, List<string> result, string s, int start)
+        {
+            for (int i = start; i < s.Length - 1; i++)
+            {
+                if (isPalindrome(s, start, i))
+                {
+
+                    result.Add(s.Substring(start, i - start + 1));
+
+                    if (isPalindrome(s, i + 1, s.Length - 1))
+                    {
+                        List<string> temp = new List<string>();
+                        temp.AddRange(result);
+                        temp.Add(s.Substring(i + 1));
+                        results.Add(temp);
+                    }
+                    getPalindrome(results, result, s, i + 1);
+                    result.RemoveAt(result.Count - 1);
+                }
+            }
+
+        }
+
+        bool isPalindrome(string s, int start, int end)
+        {
+            while (start <= end)
+            {
+                if (s[start] != s[end])
+                {
+                    return false;
+                }
+
+                start++;
+                end--;
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_131_PalindromePartitioningTest.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _131_PalindromePartitioningTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _131_PalindromePartitioning palindromePartitioning = new _131_PalindromePartitioning();
+            IList<IList<string>> result = palindromePartitioning.Partition("aab");
+
+            Assert.AreEqual(2, result.Count);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            _131_PalindromePartitioning palindromePartitioning = new _131_PalindromePartitioning();
+            IList<IList<string>> result = palindromePartitioning.Partition("a");
+
+            Assert.AreEqual(1, result.Count);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            _131_PalindromePartitioning palindromePartitioning = new _131_PalindromePartitioning();
+            IList<IList<string>> result = palindromePartitioning.Partition("bb");
+
+            Assert.AreEqual(2, result.Count);
+        }
+
+    }
+}
