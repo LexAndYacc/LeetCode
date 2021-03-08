5cb634b29b4dd6d4224021df80bf8d363ad47e90
az <westeacc@outlook.com>
2021-03-08 08:50:01 +0800

    _232_ImplementQueueUsingStacks

---
+++ LeetCode/LeetCode/Problems/_232_ImplementQueueUsingStacks.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _232_ImplementQueueUsingStacks
+    {
+        public class MyQueue
+        {
+            Stack<int> stack1;
+            Stack<int> stack2;
+
+            /** Initialize your data structure here. */
+            public MyQueue()
+            {
+                stack1 = new Stack<int>();
+                stack2 = new Stack<int>();
+            }
+
+            /** Push element x to the back of queue. */
+            public void Push(int x)
+            {
+                stack1.Push(x);
+            }
+
+            /** Removes the element from in front of queue and returns that element. */
+            public int Pop()
+            {
+                exchange();
+
+                return stack2.Pop();
+            }
+
+            /** Get the front element. */
+            public int Peek()
+            {
+                exchange();
+
+                return stack2.Peek();
+            }
+
+            void exchange()
+            {
+                if (stack2.Count == 0 && stack1.Count != 0)
+                {
+                    while (stack1.Count > 0)
+                    {
+                        stack2.Push(stack1.Pop());
+                    }
+                }
+            }
+
+            /** Returns whether the queue is empty. */
+            public bool Empty()
+            {
+                return stack1.Count == 0 && stack2.Count == 0;
+            }
+
+
+        }
+
+        /**
+         * Your MyQueue object will be instantiated and called as such:
+         * MyQueue obj = new MyQueue();
+         * obj.Push(x);
+         * int param_2 = obj.Pop();
+         * int param_3 = obj.Peek();
+         * bool param_4 = obj.Empty();
+         */
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_232_ImplementQueueUsingStacksTest.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static LeetCode.Problems._232_ImplementQueueUsingStacks;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _232_ImplementQueueUsingStacksTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MyQueue myQueue = new MyQueue();
+            myQueue.Push(1); // queue is: [1]
+            myQueue.Push(2); // queue is: [1, 2] (leftmost is front of the queue)
+            Assert.AreEqual(1, myQueue.Peek()); // return 1
+            Assert.AreEqual(1, myQueue.Pop()); // return 1, queue is [2]
+            Assert.AreEqual(false, myQueue.Empty()); // return false
+        }
+
+
+    }
+}
