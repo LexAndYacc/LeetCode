abf75ced05b711e19c1e383554672d337f6e8037
shenyuyang <westeacc@dingtalk.com>
2021-03-08 09:57:32 +0800

    _132_PalindromePartitioningII

---
+++ LeetCode/LeetCode/Problems/_132_PalindromePartitioningII.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _132_PalindromePartitioningII
+    {
+        public int MinCut(string s)
+        {
+            return mySolution(s);
+        }
+
+        int mySolution(string s)
+        {
+            bool[][] g = new bool[s.Length][];
+            for (int i = 0; i < s.Length; i++)
+            {
+                g[i] = new bool[s.Length];
+                Array.Fill(g[i], true);
+            }
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    g[i][j] = s[i] == s[j] && g[i + 1][j - 1];
+                }
+            }
+            
+            int[] dp = new int[s.Length];
+            Array.Fill(dp, int.MaxValue);
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (g[0][i])
+                {
+                    dp[i] = 0;
+                }
+                else
+                {
+                    for (int j = 0; j < i; ++j)
+                    {
+                        if (g[j + 1][i])
+                        {
+                            dp[i] = Math.Min(dp[i], dp[j] + 1);
+                        }
+                    }
+                }
+            }
+
+            return dp[s.Length - 1];
+        }
+
+
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_132_PalindromePartitioningIITest.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _132_PalindromePartitioningIITest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _132_PalindromePartitioningII palindromePartitioningII = new _132_PalindromePartitioningII();
+            int result = palindromePartitioningII.MinCut("aab");
+
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            _132_PalindromePartitioningII palindromePartitioningII = new _132_PalindromePartitioningII();
+            int result = palindromePartitioningII.MinCut("a");
+
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            _132_PalindromePartitioningII palindromePartitioningII = new _132_PalindromePartitioningII();
+            int result = palindromePartitioningII.MinCut("ab");
+
+            Assert.AreEqual(1, result);
+        }
+
+
+    }
+}
