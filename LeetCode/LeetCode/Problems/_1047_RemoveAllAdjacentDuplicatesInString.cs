be1381c675b9d36cb5bab1d8e930926b42a49d7c
az <westeacc@outlook.com>
2021-03-09 09:03:50 +0800

    _1047_RemoveAllAdjacentDuplicatesInString

---
+++ LeetCode/LeetCode/Problems/_1047_RemoveAllAdjacentDuplicatesInString.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _1047_RemoveAllAdjacentDuplicatesInString
+    {
+        public string RemoveDuplicates(string S)
+        {
+            return bestSolution(S);
+        }
+        string mySolution(string S)
+        {
+            bool flag = true;
+            while (S.Length > 1)
+            {
+                for (int i = 0; i < S.Length - 1; i++)
+                {
+                    flag = true;
+                    if (S[i] == S[i + 1])
+                    {
+                        flag = false;
+                        S = S.Remove(i, 2);
+                        if (i != 0)
+                        {
+                            i -= 2;
+                        }
+                        else
+                        {
+                            i--;
+                        }
+                    }
+                }
+
+                if (flag)
+                {
+                    break;
+                }
+            }
+            return S;
+        }
+
+        string bestSolution(string S)
+        {
+            string result = "";
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (result.Length == 0)
+                {
+                    result += S[i];
+                }
+                else
+                {
+                    if (result[result.Length - 1] == S[i])
+                    {
+                        result = result.Remove(result.Length - 1);
+                    }
+                    else
+                    {
+                        result += S[i];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_1047_RemoveAllAdjacentDuplicatesInStringTest.cs
@@ -0,0 +1,35 @@
+using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _1047_RemoveAllAdjacentDuplicatesInStringTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _1047_RemoveAllAdjacentDuplicatesInString removeAllAdjacentDuplicatesInString = new _1047_RemoveAllAdjacentDuplicatesInString();
+            string result = removeAllAdjacentDuplicatesInString.RemoveDuplicates("abbaca");
+
+            Assert.AreEqual("ca", result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            _1047_RemoveAllAdjacentDuplicatesInString removeAllAdjacentDuplicatesInString = new _1047_RemoveAllAdjacentDuplicatesInString();
+            string result = removeAllAdjacentDuplicatesInString.RemoveDuplicates("aaaaaaaa");
+
+            Assert.AreEqual("", result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            _1047_RemoveAllAdjacentDuplicatesInString removeAllAdjacentDuplicatesInString = new _1047_RemoveAllAdjacentDuplicatesInString();
+            string result = removeAllAdjacentDuplicatesInString.RemoveDuplicates("bbaacaab");
+
+            Assert.AreEqual("cb", result);
+        }
+    }
+}
