53e655f8d5dd0a035afe65174b76d73b9e746dda
az <westeacc@outlook.com>
2021-03-10 20:52:32 +0800

    _224_BasicCalculator

---
+++ LeetCode/LeetCode/Problems/_224_BasicCalculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _224_BasicCalculator
+    {
+        public int Calculate(string s)
+        {
+            return mySolution(s);
+        }
+
+        int mySolution(string s)
+        {
+            int flag = 1;
+            int result = 0;
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ')
+                {
+                    continue;
+                }
+                else if (s[i] == '(')
+                {
+                    stack.Push(flag);
+                }
+                else if (s[i] == ')')
+                {
+                    stack.Pop();
+                }
+                else if (s[i] == '+')
+                {
+                    flag = stack.Peek();
+                }
+                else if (s[i] == '-')
+                {
+                    flag = -stack.Peek();
+                }
+                else
+                {
+                    int number = 0;
+                    while (i < s.Length && char.IsDigit(s[i]))
+                    {
+                        number = number * 10 + Convert.ToInt32(s[i].ToString());
+                        i++;
+                    }
+                    i--;
+                    result += number * flag;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_224_BasicCalculatorTest.cs
@@ -0,0 +1,50 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static LeetCode.Problems._232_ImplementQueueUsingStacks;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _224_BasicCalculatorTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string s = "1 + 1";
+            _224_BasicCalculator basicCalculator = new _224_BasicCalculator();
+            int result = basicCalculator.Calculate(s);
+
+            Assert.AreEqual(2, result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string s = "2-1 + 2";
+            _224_BasicCalculator basicCalculator = new _224_BasicCalculator();
+            int result = basicCalculator.Calculate(s);
+
+            Assert.AreEqual(3, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string s = "(1+(4+5+2)-3)+(6+8)";
+            _224_BasicCalculator basicCalculator = new _224_BasicCalculator();
+            int result = basicCalculator.Calculate(s);
+
+            Assert.AreEqual(23, result);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string s = "-(3+(4+5))";
+            _224_BasicCalculator basicCalculator = new _224_BasicCalculator();
+            int result = basicCalculator.Calculate(s);
+
+            Assert.AreEqual(-12, result);
+        }
+
+    }
+}
