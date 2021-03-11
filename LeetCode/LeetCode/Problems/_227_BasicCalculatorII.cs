f5c3560f57e46a651b6e0bac1d92a98cb4c1ace0
az <westeacc@outlook.com>
2021-03-11 20:48:24 +0800

    _227_BasicCalculatorII

---
+++ LeetCode/LeetCode/Problems/_227_BasicCalculatorII.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _227_BasicCalculatorII
+    {
+        public int Calculate(string s)
+        {
+            return mySolution(s);
+        }
+
+        int mySolution(string s)
+        {
+            int result = 0;
+            int flag = 1;
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ')
+                {
+                    continue;
+                }
+                else if (s[i] == '*')
+                {
+                    result = stack.Pop();
+                    int number = 0;
+                    while (!char.IsDigit(s[i]))
+                    {
+                        i++;
+                    }
+                    while (i < s.Length && char.IsDigit(s[i]))
+                    {
+                        number = number * 10 + Convert.ToInt32(s[i].ToString());
+                        i++;
+                    }
+                    i--;
+                    stack.Push(result * number);
+                    result = 0;
+                }
+                else if (s[i] == '/')
+                {
+                    result = stack.Pop();
+                    int number = 0;
+                    while (!char.IsDigit(s[i]))
+                    {
+                        i++;
+                    }
+                    while (i < s.Length && char.IsDigit(s[i]))
+                    {
+                        number = number * 10 + Convert.ToInt32(s[i].ToString());
+                        i++;
+                    }
+                    i--;
+                    stack.Push(result / number);
+                    result = 0;
+                }
+                else if (s[i] == '+')
+                {
+                    flag = 1;
+                }
+                else if (s[i] == '-')
+                {
+                    flag = -1;
+                }
+                else
+                {
+                    int number = 0;
+                    while (i < s.Length && char.IsDigit(s[i]))
+                    {
+                        number = number * 10 + Convert.ToInt32(s[i].ToString());
+                        i++;
+                    }
+                    i--;
+                    stack.Push(number * flag);
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                result += stack.Pop();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_227_BasicCalculatorIITest.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static LeetCode.Problems._232_ImplementQueueUsingStacks;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _227_BasicCalculatorIITest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string s = "3+2*2";
+            _227_BasicCalculatorII basicCalculatorII = new _227_BasicCalculatorII();
+            int result = basicCalculatorII.Calculate(s);
+
+            Assert.AreEqual(7, result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string s = "3/2";
+            _227_BasicCalculatorII basicCalculatorII = new _227_BasicCalculatorII();
+            int result = basicCalculatorII.Calculate(s);
+
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string s = " 3+5 / 2";
+            _227_BasicCalculatorII basicCalculatorII = new _227_BasicCalculatorII();
+            int result = basicCalculatorII.Calculate(s);
+
+            Assert.AreEqual(5, result);
+        }
+
+    }
+}
