3f443f8f7eedb95dbee68689cffd61ffc0df49d8
az <westeacc@outlook.com>
2021-03-23 09:47:37 +0800

    _341_FlattenNestedListIterator

---
+++ LeetCode/LeetCode/Problems/_341_FlattenNestedListIterator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _341_FlattenNestedListIterator
+    {
+        public class NestedIterator
+        {
+            Stack<(int, IList<NestedInteger>)> stack;
+
+            public NestedIterator(IList<NestedInteger> nestedList)
+            {
+                stack = new Stack<(int, IList<NestedInteger>)>();
+                stack.Push((0, nestedList));
+            }
+
+            public bool HasNext()
+            {
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+
+                var top = stack.Pop();
+                int num = top.Item1;
+                IList<NestedInteger> list = top.Item2;
+
+                if (list.Count == num)
+                {
+                    return HasNext();
+                }
+
+                if (list[num].IsInteger())
+                {
+                    stack.Push(top);
+                    return true;
+                }
+                else
+                {
+                    top.Item1++;
+                    stack.Push(top);
+                    stack.Push((0, list[num].GetList()));
+                    return HasNext();
+                }
+
+            }
+
+            public int Next()
+            {
+                var top = stack.Pop();
+                int num = top.Item1;
+                IList<NestedInteger> list = top.Item2;
+                stack.Push((num + 1, list));
+
+                return list[num].GetInteger();
+            }
+        }
+
+        public interface NestedInteger
+        {
+
+            // @return true if this NestedInteger holds a single integer, rather than a nested list.
+            bool IsInteger();
+
+            // @return the single integer that this NestedInteger holds, if it holds a single integer
+            // Return null if this NestedInteger holds a nested list
+            int GetInteger();
+
+            // @return the nested list that this NestedInteger holds, if it holds a nested list
+            // Return null if this NestedInteger holds a single integer
+            IList<NestedInteger> GetList();
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_341_FlattenNestedListIteratorTest.cs
@@ -0,0 +1,89 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static LeetCode.Problems._341_FlattenNestedListIterator;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _341_FlattenNestedListIteratorTest
+    {
+        class NestedIntegers : NestedInteger
+        {
+            int value;
+            IList<NestedInteger> list = null;
+
+            public NestedIntegers(int value)
+            {
+                this.value = value;
+            }
+
+            public NestedIntegers(IList<NestedInteger> list)
+            {
+                this.list = list;
+            }
+
+            public int GetInteger()
+            {
+                return value;
+            }
+
+            public IList<NestedInteger> GetList()
+            {
+                return list;
+            }
+
+            public bool IsInteger()
+            {
+                return list == null;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IList<NestedInteger> list = new List<NestedInteger>();
+            list.Add(new NestedIntegers(new List<NestedInteger>() {
+                new NestedIntegers(1),
+                new NestedIntegers(1)
+            }));
+            list.Add(new NestedIntegers(2));
+            list.Add(new NestedIntegers(new List<NestedInteger>() {
+                new NestedIntegers(1),
+                new NestedIntegers(1)
+            }));
+
+            NestedIterator nestedIterator = new NestedIterator(list);
+
+            int[] expected = { 1, 1, 2, 1, 1 };
+
+            for (int i = 0; nestedIterator.HasNext() || i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], nestedIterator.Next());
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            IList<NestedInteger> list = new List<NestedInteger>();
+            list.Add(new NestedIntegers(1));
+            list.Add(new NestedIntegers(new List<NestedInteger>() {
+                new NestedIntegers(4),
+                new NestedIntegers(new List<NestedInteger>() {
+                    new NestedIntegers(6)
+                })
+            }));
+
+            NestedIterator nestedIterator = new NestedIterator(list);
+
+            int[] expected = { 1, 4, 6 };
+
+            for (int i = 0; nestedIterator.HasNext() || i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], nestedIterator.Next());
+            }
+        }
+
+    }
+}
