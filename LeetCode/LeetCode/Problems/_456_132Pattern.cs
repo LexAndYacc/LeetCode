2b794aedac87a4ab9fb432e86d2e153db4ba609b
az <westeacc@outlook.com>
2021-03-24 15:03:22 +0800

    _456_132Pattern

---
+++ LeetCode/LeetCode/Problems/_456_132Pattern.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _456_132Pattern
+    {
+        public bool Find132pattern(int[] nums)
+        {
+            return bestSolution(nums);
+        }
+
+        bool bestSolution(int[] nums)
+        {
+            int n = nums.Length;
+            Stack<int> candidateK = new Stack<int>();
+            candidateK.Push(nums[n - 1]);
+            int maxK = int.MinValue;
+
+            for (int i = n - 2; i >= 0; --i)
+            {
+                if (nums[i] < maxK)
+                {
+                    return true;
+                }
+                while (candidateK.Count != 0 && nums[i] > candidateK.Peek())
+                {
+                    maxK = candidateK.Pop();
+                }
+                if (nums[i] > maxK)
+                {
+                    candidateK.Push(nums[i]);
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_456_132PatternTest.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _456_132PatternTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            _456_132Pattern pattern = new _456_132Pattern();
+            bool result = pattern.Find132pattern(nums);
+
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] nums = { 3, 1, 4, 2 };
+            _456_132Pattern pattern = new _456_132Pattern();
+            bool result = pattern.Find132pattern(nums);
+
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] nums = { -1, 3, 2, 0 };
+            _456_132Pattern pattern = new _456_132Pattern();
+            bool result = pattern.Find132pattern(nums);
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
