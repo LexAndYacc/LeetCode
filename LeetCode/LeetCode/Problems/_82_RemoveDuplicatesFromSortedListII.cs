a176a0a44b49877d136e4a057fadfa396f906fae
az <westeacc@outlook.com>
2021-03-25 15:56:47 +0800

    _82_RemoveDuplicatesFromSortedListII

---
+++ LeetCode/LeetCode/Problems/_82_RemoveDuplicatesFromSortedListII.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _82_RemoveDuplicatesFromSortedListII
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            return mySolution(head);
+        }
+
+        ListNode mySolution(ListNode head)
+        {
+            ListNode p = new ListNode(666, head);
+            ListNode last = p;
+            int flag = 666;
+
+            while (head != null && head.next != null)
+            {
+                if (head.val == head.next.val)
+                {
+                    flag = head.val;
+                    last.next = head.next.next;
+                    head = head.next.next;
+                }
+                else
+                {
+                    flag = 666;
+                    last = head;
+                    head = head.next;
+                }
+                while (head != null)
+                {
+                    if (head.val == flag)
+                    {
+                        last.next = head.next;
+                        head = head.next;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+            return p.next;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_82_RemoveDuplicatesFromSortedListIITest.cs
@@ -0,0 +1,66 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static LeetCode.Problems._82_RemoveDuplicatesFromSortedListII;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _82_RemoveDuplicatesFromSortedListIITest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5)))))));
+            _82_RemoveDuplicatesFromSortedListII removeDuplicatesFromSortedListII = new _82_RemoveDuplicatesFromSortedListII();
+            ListNode result = removeDuplicatesFromSortedListII.DeleteDuplicates(head);
+
+            ListNode expected = new ListNode(1, new ListNode(2, new ListNode(5)));
+
+            while (expected != null)
+            {
+                Assert.AreEqual(expected.val, result.val);
+
+                expected = expected.next;
+                result = result.next;
+            }
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            ListNode head = new ListNode(1, new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3)))));
+            _82_RemoveDuplicatesFromSortedListII removeDuplicatesFromSortedListII = new _82_RemoveDuplicatesFromSortedListII();
+            ListNode result = removeDuplicatesFromSortedListII.DeleteDuplicates(head);
+
+            ListNode expected = new ListNode(2, new ListNode(3));
+
+            while (expected != null)
+            {
+                Assert.AreEqual(expected.val, result.val);
+
+                expected = expected.next;
+                result = result.next;
+            }
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            ListNode head = new ListNode(1, new ListNode(1));
+            _82_RemoveDuplicatesFromSortedListII removeDuplicatesFromSortedListII = new _82_RemoveDuplicatesFromSortedListII();
+            ListNode result = removeDuplicatesFromSortedListII.DeleteDuplicates(head);
+
+            ListNode expected = null;
+
+            while (expected != null)
+            {
+                Assert.AreEqual(expected.val, result.val);
+
+                expected = expected.next;
+                result = result.next;
+            }
+        }
+    }
+}
