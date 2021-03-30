e8cf000815a15dc6619577e8eb600e3034c4cce2
az <westeacc@outlook.com>
2021-03-30 17:05:11 +0800

    _74_SearchA2dMatrix

---
+++ LeetCode/LeetCode/Problems/_74_SearchA2dMatrix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _74_SearchA2dMatrix
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            return mySolution(matrix, target);
+        }
+
+        bool mySolution(int[][] matrix, int target)
+        {
+            foreach (var i in matrix)
+            {
+                foreach (var j in i)
+                {
+                    if (j == target)
+                    {
+                        return true;
+                    }
+                    else if (j > target)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_74_SearchA2dMatrixTest.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static LeetCode.Problems._706_DesignHashmap;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _74_SearchA2dMatrixTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[][] matrix =
+            {
+                new int[]{1,3,5,7},
+                new int[]{10,11,16,20},
+                new int[]{23,30,34,60}
+            };
+            int target = 3;
+            _74_SearchA2dMatrix searchA2DMatrix = new _74_SearchA2dMatrix();
+            bool result = searchA2DMatrix.SearchMatrix(matrix, target);
+
+            Assert.AreEqual(true, result);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[][] matrix =
+            {
+                new int[]{1,3,5,7},
+                new int[]{10,11,16,20},
+                new int[]{23,30,34,60}
+            };
+            int target = 13;
+            _74_SearchA2dMatrix searchA2DMatrix = new _74_SearchA2dMatrix();
+            bool result = searchA2DMatrix.SearchMatrix(matrix, target);
+
+            Assert.AreEqual(false, result);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[][] matrix =
+            {
+                new int[]{1}
+            };
+            int target = 0;
+            _74_SearchA2dMatrix searchA2DMatrix = new _74_SearchA2dMatrix();
+            bool result = searchA2DMatrix.SearchMatrix(matrix, target);
+
+            Assert.AreEqual(false, result);
+
+        }
+    }
+}
