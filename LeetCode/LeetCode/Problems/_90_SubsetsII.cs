31072fc0fa22cf841bbc1a764bd464af447c1d73
az <westeacc@outlook.com>
2021-03-31 15:45:42 +0800

    _90_SubsetsII

---
+++ LeetCode/LeetCode/Problems/_90_SubsetsII.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _90_SubsetsII
+    {
+        public IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            return bestSolution(nums);
+        }
+
+        IList<IList<int>> bestSolution(int[] nums)
+        {
+            Array.Sort(nums);
+            IList<IList<int>> list = new List<IList<int>>();
+            list.Add(new List<int>());
+            List<int> l = new List<int>();
+
+            int lastpos = -1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int count = list.Count;
+                int start = 0;
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    start = lastpos;
+                }
+                for (int j = start; j < count; j++)
+                {
+                    List<int> ll = new List<int>();
+                    for (int r = 0; r < list.ElementAt(j).Count; r++)
+                    {
+                        ll.Add(list.ElementAt(j).ElementAt(r));
+                    }
+                    ll.Add(nums[i]);
+                    list.Add(ll);
+                }
+                lastpos = count;
+            }
+            return list;
+        }
+
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_90_SubsetsIITest.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static LeetCode.Problems._99_ReverseLinkedListII;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _90_SubsetsIITest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] nums = { 1, 2, 2 };
+            _90_SubsetsII subsetsII = new _90_SubsetsII();
+            IList<IList<int>> result = subsetsII.SubsetsWithDup(nums);
+
+            Assert.AreEqual(6, result.Count);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] nums = { 0 };
+            _90_SubsetsII subsetsII = new _90_SubsetsII();
+            IList<IList<int>> result = subsetsII.SubsetsWithDup(nums);
+
+            Assert.AreEqual(2, result.Count);
+        }
+    }
+}
