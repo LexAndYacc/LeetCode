d509bded4169e645aa351255b08c061283fd9275
az <westeacc@outlook.com>
2021-04-12 15:10:36 +0800

    _179_LargestNumber

---
+++ LeetCode/LeetCode/Problems/_179_LargestNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _179_LargestNumber
+    {
+        public string LargestNumber(int[] nums)
+        {
+            return mySolution(nums);
+        }
+
+        string mySolution(int[] nums)
+        {
+            string result = string.Empty;
+            Array.Sort(nums, (x, y) =>
+            {
+                string xStr = x.ToString();
+                string yStr = y.ToString();
+
+                return -(xStr + yStr).CompareTo(yStr + xStr);
+            });
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result += nums[i].ToString();
+            }
+
+            result = result.TrimStart('0');
+            if (result.Length == 0)
+            {
+                result = "0";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_179_LargestNumberTest.cs
@@ -0,0 +1,58 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _179_LargestNumberTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] nums = { 10, 2 };
+            _179_LargestNumber largestNumber = new _179_LargestNumber();
+            string result = largestNumber.LargestNumber(nums);
+
+            Assert.AreEqual("210", result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] nums = { 3, 30, 34, 5, 9 };
+            _179_LargestNumber largestNumber = new _179_LargestNumber();
+            string result = largestNumber.LargestNumber(nums);
+
+            Assert.AreEqual("9534330", result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] nums = { 1 };
+            _179_LargestNumber largestNumber = new _179_LargestNumber();
+            string result = largestNumber.LargestNumber(nums);
+
+            Assert.AreEqual("1", result);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int[] nums = { 10 };
+            _179_LargestNumber largestNumber = new _179_LargestNumber();
+            string result = largestNumber.LargestNumber(nums);
+
+            Assert.AreEqual("10", result);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int[] nums = { 34323, 3432 };
+            _179_LargestNumber largestNumber = new _179_LargestNumber();
+            string result = largestNumber.LargestNumber(nums);
+
+            Assert.AreEqual("343234323", result);
+        }
+
+    }
+}
