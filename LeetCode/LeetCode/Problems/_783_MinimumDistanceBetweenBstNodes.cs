cf7b40aefa1bfbd8f9529f3945933151ad32bdf9
az <westeacc@outlook.com>
2021-04-13 20:05:52 +0800

    _783_MinimumDistanceBetweenBstNodes

---
+++ LeetCode/LeetCode/Problems/_783_MinimumDistanceBetweenBstNodes.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _783_MinimumDistanceBetweenBstNodes
+    {
+        public int MinDiffInBST(TreeNode root)
+        {
+            return mySolution(root);
+        }
+
+        int mySolution(TreeNode root)
+        {
+            int result = int.MaxValue;
+
+
+            int pre = -1;
+            TreeNode precessor = null;
+            while (root != null)
+            {
+                if (root.left != null)
+                {
+                    precessor = root.left;
+                    while (precessor.right != null && precessor.right != root)
+                        precessor = precessor.right;
+                    if (precessor.right == null)
+                    {
+                        precessor.right = root;
+                        root = root.left;
+                    }
+                    else
+                    {
+                        if (pre == -1)
+                        {
+                            pre = root.val;
+                        }
+                        else
+                        {
+                            result = Math.Min(result, Math.Abs(pre - root.val));
+                            pre = root.val;
+                        }
+                        root = root.right;
+                    }
+                }
+                else
+                {
+                    if (pre == -1)
+                    {
+                        pre = root.val;
+                    }
+                    else
+                    {
+                        result = Math.Min(result, Math.Abs(pre - root.val));
+                        pre = root.val;
+                    }
+                    root = root.right;
+                }
+            }
+
+            return result;
+        }
+        
+
+
+
+        #region 内部类
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        #endregion
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_783_MinimumDistanceBetweenBstNodesTest.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static LeetCode.Problems._783_MinimumDistanceBetweenBstNodes;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _783_MinimumDistanceBetweenBstNodesTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(6));
+            _783_MinimumDistanceBetweenBstNodes minimumDistanceBetweenBstNodes = new _783_MinimumDistanceBetweenBstNodes();
+            int result = minimumDistanceBetweenBstNodes.MinDiffInBST(root);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            TreeNode root = new TreeNode(1, new TreeNode(0), new TreeNode(48, new TreeNode(12), new TreeNode(49)));
+            _783_MinimumDistanceBetweenBstNodes minimumDistanceBetweenBstNodes = new _783_MinimumDistanceBetweenBstNodes();
+            int result = minimumDistanceBetweenBstNodes.MinDiffInBST(root);
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
