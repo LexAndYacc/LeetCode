61fee4d38ef7ab17e7a6a4f637cafac539424d29
az <westeacc@outlook.com>
2021-04-14 09:43:04 +0800

    _208_ImplementTriePrefixTree

---
+++ LeetCode/LeetCode/Problems/_208_ImplementTriePrefixTree.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _208_ImplementTriePrefixTree
+    {
+        public class Trie
+        {
+            public bool IsEnd { get; set; }
+            public Trie[] Next { get; set; }
+
+            /** Initialize your data structure here. */
+            public Trie()
+            {
+                IsEnd = false;
+                Next = new Trie[26];
+            }
+
+            /** Inserts a word into the trie. */
+            public void Insert(string word)
+            {
+                Trie node = this;
+                foreach (char c in word)
+                {
+                    if (node.Next[c - 'a'] == null)
+                    {
+                        node.Next[c - 'a'] = new Trie();
+                    }
+                    node = node.Next[c - 'a'];
+                }
+                node.IsEnd = true;
+            }
+
+            /** Returns if the word is in the trie. */
+            public bool Search(string word)
+            {
+                Trie node = this;
+                foreach (char c in word)
+                {
+                    node = node.Next[c - 'a'];
+                    if (node == null)
+                    {
+                        return false;
+                    }
+                }
+                return node.IsEnd;
+            }
+
+            /** Returns if there is any word in the trie that starts with the given prefix. */
+            public bool StartsWith(string prefix)
+            {
+                Trie node = this;
+                foreach (char c in prefix)
+                {
+                    node = node.Next[c - 'a'];
+                    if (node == null)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_208_ImplementTriePrefixTreeTest.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static LeetCode.Problems._208_ImplementTriePrefixTree;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _208_ImplementTriePrefixTreeTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Trie trie = new Trie();
+            trie.Insert("apple");
+            Assert.AreEqual(true, trie.Search("apple"));   // 返回 True
+            Assert.AreEqual(false, trie.Search("app"));     // 返回 False
+            Assert.AreEqual(true, trie.StartsWith("app")); // 返回 True
+            trie.Insert("app");
+            Assert.AreEqual(true, trie.Search("app"));     // 返回 True
+        }
+    }
+}
