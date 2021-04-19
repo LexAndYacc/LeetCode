c4e88a26189a54355e3bae40085a7de20014601c
az <westeacc@outlook.com>
2021-04-19 19:55:17 +0800

    _27_RemoveElement

---
+++ LeetCode/LeetCode/Problems/_27_RemoveElement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class _27_RemoveElement
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            return mySolution(nums, val);
+        }
+
+        int mySolution(int[] nums,int val)
+        {
+            int left = 0;
+            int right = nums.Length;
+            while (left < right)
+            {
+                if (nums[left] == val)
+                {
+                    nums[left] = nums[right - 1];
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
+            }
+            return left;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/Problems/_27_RemoveElementTest.cs
@@ -0,0 +1,43 @@
+﻿using LeetCode.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCodeTest.Problems
+{
+    [TestClass]
+    public class _27_RemoveElementTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] nums = { 3, 2, 2, 3 };
+            _27_RemoveElement removeElement = new _27_RemoveElement();
+            int result = removeElement.RemoveElement(nums, 3);
+
+            Assert.AreEqual(2, result);
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+            _27_RemoveElement removeElement = new _27_RemoveElement();
+            int result = removeElement.RemoveElement(nums, 2);
+
+            Assert.AreEqual(5, result);
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] nums = { 1, 1, 1, 1, 1 };
+            _27_RemoveElement removeElement = new _27_RemoveElement();
+            int result = removeElement.RemoveElement(nums, 1);
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
