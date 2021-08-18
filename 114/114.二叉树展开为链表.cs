fd08fc90a7a6a7379e5e395f227e6d5c2c28949a
az <westeacc@outlook.com>
2021-08-18 17:58:41 +0800

    114

---
+++ 114/114.二叉树展开为链表.cs
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=csharp
+ *
+ * [114] 二叉树展开为链表
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public void Flatten(TreeNode root)
+    {
+        if (root == null) return;
+
+        Flatten(root.left);
+        Flatten(root.right);
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        root.left = null;
+        root.right = left;
+
+        TreeNode p = root;
+        while (p.right != null)
+        {
+            p = p.right;
+        }
+        p.right = right;
+    }
+}
+// @lc code=end
+
